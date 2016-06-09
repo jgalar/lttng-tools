97581f2e7457771fc29693dba97dc72abeed0be1
Jonathan Rajotte <jonathan.rajotte-julien@efficios.com>
2016-06-09 11:15:59 -0400

    Fix format and variable name create.c

--- src/bin/lttng/commands/create.c
+++ src/bin/lttng/commands/create.c
@@ -454,7 +454,7 @@ static int parse_template (struct config_document *template,
 		char **shm_path)
 {
 	int ret = 0;
-	int printed_byte;
+	int printed_bytes;
 	char *raw_value = NULL;
 
 	assert(template);
@@ -491,8 +491,8 @@ static int parse_template (struct config_document *template,
 
 			if (strlen(raw_value) > 0) {
 				*output_type = OUTPUT_LOCAL;
-				printed_byte = asprintf(url, "file://%s", raw_value);
-				if (printed_byte < 0) {
+				printed_bytes = asprintf(url, "file://%s", raw_value);
+				if (printed_bytes < 0) {
 					ret = -1;
 					goto error;
 				}
@@ -528,8 +528,8 @@ static int parse_template (struct config_document *template,
 
 			if (strlen(raw_value) > 0) {
 				*output_type = OUTPUT_LOCAL;
-				printed_byte = asprintf(url, "file://%s", raw_value);
-				if (printed_byte < 0) {
+				printed_bytes = asprintf(url, "file://%s", raw_value);
+				if (printed_bytes < 0) {
 					ret = -1;
 					goto error;
 				}
@@ -575,7 +575,7 @@ static int create_session_from_template(struct config_document *template,
 		const char *datetime)
 {
 	int ret = CMD_SUCCESS;
-	int printed_byte;
+	int printed_bytes;
 	struct config_element *temp_element = NULL;
 	struct config_element *temp_element_child = NULL;
 	char tmp_ctrl_uri[PATH_MAX];
@@ -616,8 +616,8 @@ static int create_session_from_template(struct config_document *template,
 	 */
 	if (session_type == SESSION_LIVE) {
 		if (config_document_element_exist(template, "/sessions/session/attributes/live_timer_interval")) {
-			printed_byte = asprintf(&tmp_string, "%d", live_timer);
-			if (printed_byte < 0) {
+			printed_bytes = asprintf(&tmp_string, "%d", live_timer);
+			if (printed_bytes < 0) {
 				ERR("Asprintf failed for live timer");
 				ret = CMD_ERROR;
 				goto error;
@@ -837,7 +837,7 @@ error:
 static int create_session(void)
 {
 	int ret;
-	int printed_byte;
+	int printed_bytes;
 
 
 	/* Template */
@@ -948,24 +948,24 @@ static int create_session(void)
 			goto error;
 		}
 
-		printed_byte = asprintf(&session_name_date, "%s-%s", base_session_name, datetime);
-		if (printed_byte < 0) {
+		printed_bytes = asprintf(&session_name_date, "%s-%s", base_session_name, datetime);
+		if (printed_bytes < 0) {
 			PERROR("Asprintf session name");
 			ret = CMD_ERROR;
 			goto error;
 		}
 		DBG("Session name from command option set to %s", base_session_name);
 	} else if (base_session_name) {
-		printed_byte = asprintf(&session_name_date, "%s-%s", base_session_name, datetime);
-		if (printed_byte < 0) {
+		printed_bytes = asprintf(&session_name_date, "%s-%s", base_session_name, datetime);
+		if (printed_bytes < 0) {
 			PERROR("Asprintf session name");
 			ret = CMD_ERROR;
 			goto error;
 		}
 	} else {
 		/* Generate a name */
-		printed_byte = asprintf(&base_session_name, DEFAULT_SESSION_NAME "-%s", datetime);
-		if (printed_byte < 0) {
+		printed_bytes = asprintf(&base_session_name, DEFAULT_SESSION_NAME "-%s", datetime);
+		if (printed_bytes < 0) {
 			PERROR("Asprintf generated session name");
 			ret = CMD_ERROR;
 			goto error;
@@ -1001,8 +1001,8 @@ static int create_session(void)
 		}
 
 		/* Create URL string from the local file system path */
-		printed_byte = asprintf(&temp_url, "file://%s", traces_path);
-		if (printed_byte < 0) {
+		printed_bytes = asprintf(&temp_url, "file://%s", traces_path);
+		if (printed_bytes < 0) {
 			PERROR("asprintf url path");
 			ret = CMD_FATAL;
 			goto error;
@@ -1034,11 +1034,11 @@ static int create_session(void)
 				goto error;
 			}
 
-			printed_byte = asprintf(&tmp_url,
+			printed_bytes = asprintf(&tmp_url,
 					"file://%s/" DEFAULT_TRACE_DIR_NAME "/%s",
 					tmp_home_path, session_name_date);
 
-			if (printed_byte < 0) {
+			if (printed_bytes < 0) {
 				PERROR("asprintf trace dir name");
 				ret = CMD_FATAL;
 				goto error;
@@ -1048,8 +1048,8 @@ static int create_session(void)
 			break;
 		case SESSION_LIVE:
 			/* Default to a net output */
-			printed_byte = asprintf(&tmp_url, "net://127.0.0.1");
-			if (printed_byte < 0) {
+			printed_bytes = asprintf(&tmp_url, "net://127.0.0.1");
+			if (printed_bytes < 0) {
 				PERROR("asprintf default live URL");
 				ret = CMD_FATAL;
 				goto error;
@@ -1069,8 +1069,8 @@ static int create_session(void)
 	if (opt_shm_path) {
 		/* Overwrite shm_path so clear any previously defined one */
 		free(base_shm_path);
-		printed_byte = asprintf(&base_shm_path, "%s/%s", opt_shm_path, session_name_date);
-		if (printed_byte < 0) {
+		printed_bytes = asprintf(&base_shm_path, "%s/%s", opt_shm_path, session_name_date);
+		if (printed_bytes < 0) {
 			PERROR("asprintf shm_path");
 			ret = CMD_FATAL;
 			goto error;
@@ -1085,6 +1085,7 @@ static int create_session(void)
 	}
 
 	/* Get output type from urls */
+	/* TODO: Find a better way of inferring output type */
 	if (base_url) {
 		/* Get lttng uris from single url */
 		uri_array_size = uri_parse_str_urls(base_url, NULL, &uris);
@@ -1130,7 +1131,7 @@ static int create_session(void)
 				base_shm_path,
 				datetime);
 	} else {
-		ret = create_session_basic (base_session_name,
+		ret = create_session_basic(base_session_name,
 				base_session_type,
 				base_live_timer,
 				base_output_type,
@@ -1144,7 +1145,7 @@ static int create_session(void)
 		goto error;
 	}
 
-	ret = generate_output (base_session_name,
+	ret = generate_output(base_session_name,
 			base_session_type,
 			base_live_timer,
 			base_output_type,
