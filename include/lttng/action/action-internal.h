a87af0fdea0d8ca540032510cca548d222fa295a
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2017-02-27 13:38:58 -0500

    lttng-ctl: Implement the action interface
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

---
+++ include/lttng/action/action-internal.h
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2017 - Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License, version 2.1 only,
+ * as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#ifndef LTTNG_ACTION_INTERNAL_H
+#define LTTNG_ACTION_INTERNAL_H
+
+#include <lttng/action/action.h>
+#include <common/macros.h>
+#include <stdbool.h>
+
+typedef bool (*action_validate_cb)(struct lttng_action *action);
+typedef void (*action_destroy_cb)(struct lttng_action *action);
+
+struct lttng_action {
+	enum lttng_action_type type;
+	action_validate_cb validate;
+	action_destroy_cb destroy;
+};
+
+LTTNG_HIDDEN
+bool lttng_action_validate(struct lttng_action *action);
+
+#endif /* LTTNG_ACTION_INTERNAL_H */
---
+++ src/lib/lttng-ctl/action.c
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 - Jérémie Galarneau <jeremie.galarneau@efficios.com>
+ *
+ * This library is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License, version 2.1 only,
+ * as published by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+#include <lttng/action/action-internal.h>
+#include <assert.h>
+
+enum lttng_action_type lttng_action_get_type(struct lttng_action *action)
+{
+	return action ? action->type : LTTNG_ACTION_TYPE_UNKNOWN;
+}
+
+void lttng_action_destroy(struct lttng_action *action)
+{
+	if (!action) {
+		return;
+	}
+
+	assert(action->destroy);
+	action->destroy(action);
+}
+
+LTTNG_HIDDEN
+bool lttng_action_validate(struct lttng_action *action)
+{
+	bool valid;
+
+	if (!action) {
+		valid = false;
+		goto end;
+	}
+
+	if (!action->validate) {
+		/* Sub-class guarantees that it can never be invalid. */
+		valid = true;
+		goto end;
+	}
+
+	valid = action->validate(action);
+end:
+	return valid;
+}
