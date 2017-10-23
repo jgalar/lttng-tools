0f6a4bf0af5c769d846414166c3f153c18e38283
Julien Desfossez <jdesfossez@efficios.com>
2017-10-23 14:08:05 -0400

    all tests are now working
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>

--- tests/regression/tools/rotation/rotate_utils.sh
+++ tests/regression/tools/rotation/rotate_utils.sh
@@ -1,6 +1,6 @@
 function validate_test_chunks ()
 {
-	TRACE_PATH=$1
+	local_path=$1
 	today=$2
 	app_path=$3
 	domain=$4
@@ -9,51 +9,53 @@ function validate_test_chunks ()
 	chunk_pattern="${today}-[0-9][0-9][0-9][0-9][0-9][0-9]-${today}-[0-9][0-9][0-9][0-9][0-9][0-9]"
 
 	# Check if the 3 chunk folders exist and they contain a ${app_path}/metadata file.
-	ls $TRACE_PATH/${chunk_pattern}-1/${app_path}/metadata >/dev/null
+	ls $local_path/${chunk_pattern}-1/${app_path}/metadata >/dev/null
 	ok $? "Chunk 1 exists"
-	ls $TRACE_PATH/${chunk_pattern}-2/${app_path}/metadata >/dev/null
+	ls $local_path/${chunk_pattern}-2/${app_path}/metadata >/dev/null
 	ok $? "Chunk 2 exists"
-	ls $TRACE_PATH/${chunk_pattern}-3/${domain} >/dev/null
+	ls $local_path/${chunk_pattern}-3/${domain} >/dev/null
 	ok $? "Chunk 3 exists"
 
 	# Make sure we don't have anything else in the first 2 chunk directories
 	# besides the kernel folder.
-	nr_stale=$(\ls $TRACE_PATH/${chunk_pattern}-1 | grep -v $domain | wc -l)
+	nr_stale=$(\ls $local_path/${chunk_pattern}-1 | grep -v $domain | wc -l)
 	ok $nr_stale "No stale folders in chunk 1 directory"
-	nr_stale=$(\ls $TRACE_PATH/${chunk_pattern}-2 | grep -v $domain | wc -l)
+	nr_stale=$(\ls $local_path/${chunk_pattern}-2 | grep -v $domain | wc -l)
 	ok $nr_stale "No stale folders in chunk 2 directory"
 
 	# We expect a session of 30 events
-	validate_trace_count $EVENT_NAME $TRACE_PATH 30
+	validate_trace_count $EVENT_NAME $local_path 30
 
 	# Chunk 1: 10 events
-	validate_trace_count $EVENT_NAME $TRACE_PATH/${chunk_pattern}-1 10
+	validate_trace_count $EVENT_NAME $local_path/${chunk_pattern}-1 10
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
-		rm -rf $TRACE_PATH/${chunk_pattern}-1
+		rm -rf $local_path/${chunk_pattern}-1
 	fi
 
 	# Chunk 2: 20 events
-	validate_trace_count $EVENT_NAME $TRACE_PATH/${chunk_pattern}-2 20
+	validate_trace_count $EVENT_NAME $local_path/${chunk_pattern}-2 20
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
-		rm -rf $TRACE_PATH/${chunk_pattern}-2
+		rm -rf $local_path/${chunk_pattern}-2
 	fi
 
 	# Chunk 3: 0 event
-	validate_trace_empty $TRACE_PATH/${chunk_pattern}-3
+	validate_trace_empty $local_path/${chunk_pattern}-3
 	if [ $? -eq 0 ]; then
 		# Only delete if successful
-		rm -rf $TRACE_PATH/${chunk_pattern}-3
+		rm -rf $local_path/${chunk_pattern}-3
 	fi
 
 	# The session folder after all chunks have been removed is empty
-	test -z "$(\ls -A $TRACE_PATH)"
+	test -z "$(\ls -A $local_path)"
 	empty=$?
 	ok $empty "Trace folder is now empty"
 	if [ $empty -eq 0 ]; then
  	# Only delete if successful
-		rm -rf $TRACE_PATH/
+		rm -rf $local_path/
+	else
+		find $local_path
 	fi
 }
 
