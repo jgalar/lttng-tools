5bef115c61375dbd3c976469491c4b1207483161
Julien Desfossez <jdesfossez@efficios.com>
2018-02-09 14:53:32 -0500

    Size-based rotation
    The user can now configure the desired size of each chunk, every time a
    chunk is bigger than the specified size, a rotation is automatically
    started. The size of a chunk is measured by polling from the monitoring
    thread, so the accuracy depends on the monitoring sampling rate.
    
    Signed-off-by: Julien Desfossez <jdesfossez@efficios.com>
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/cmd.c
+++ src/bin/lttng-sessiond/cmd.c
@@ -2935,7 +2935,8 @@ error:
  *
  * Called with session lock held.
  */
-int cmd_destroy_session(struct ltt_session *session, int wpipe)
+int cmd_destroy_session(struct ltt_session *session, int wpipe,
+		struct notification_thread_handle *notification_thread_handle)
 {
 	int ret;
 	struct ltt_ust_session *usess;
@@ -2955,6 +2956,11 @@ int cmd_destroy_session(struct ltt_session *session, int wpipe)
 
 	if (session->rotate_timer_enabled) {
 		sessiond_rotate_timer_stop(session);
+	}
+
+	if (session->rotate_size) {
+		unsubscribe_session_usage_rotation(session, notification_thread_handle);
+		session->rotate_size = 0;
 	}
 
 	/*
@@ -4661,7 +4667,8 @@ end:
  * Return 0 on success or else a LTTNG_ERR code.
  */
 int cmd_rotation_set_schedule(struct ltt_session *session,
-		uint64_t timer_us, uint64_t size)
+		uint64_t timer_us, uint64_t size,
+		struct notification_thread_handle *notification_thread_handle)
 {
 	int ret;
 
@@ -4685,6 +4692,14 @@ int cmd_rotation_set_schedule(struct ltt_session *session,
 		goto end;
 	}
 
+	if (size && size != -1ULL && session->rotate_size) {
+		ret = LTTNG_ERR_ROTATION_SIZE_IS_SET;
+		goto end;
+	} else if (size == -1ULL && !session->rotate_size) {
+		ret = LTTNG_ERR_ROTATION_SIZE_IS_SET;
+		goto end;
+	}
+
 	if (timer_us && !session->rotate_timer_period) {
 		session->rotate_timer_period = timer_us;
 		/*
@@ -4702,6 +4717,23 @@ int cmd_rotation_set_schedule(struct ltt_session *session,
 	} else if (timer_us == -1ULL && session->rotate_timer_period > 0) {
 		sessiond_rotate_timer_stop(session);
 		session->rotate_timer_period = 0;
+	}
+
+	if (size > 0) {
+		if (size == -1ULL) {
+			unsubscribe_session_usage_rotation(session,
+					notification_thread_handle);
+			session->rotate_size = 0;
+		} else {
+			ret = subscribe_session_usage_rotation(session, size,
+					notification_thread_handle);
+			if (ret) {
+				PERROR("Subscribe to session usage");
+				ret = LTTNG_ERR_UNK;
+				goto end;
+			}
+			session->rotate_size = size;
+		}
 	}
 
 	ret = LTTNG_OK;
