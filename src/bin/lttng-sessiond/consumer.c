7b68ef12a939df9233f5a668ecafd6280e15e67c
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2019-03-04 22:16:10 -0500

    relayd: send sessiond uuid and session id as part of create session
    Send the sessiond's instance UUID and session id as part of the
    relay daemon control protocol's CREATE_SESSION command.
    
    Those attributes will be used to uniquely identify trace chunks
    in follow-up commits.
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/bin/lttng-sessiond/consumer.c
+++ src/bin/lttng-sessiond/consumer.c
@@ -36,6 +36,7 @@
 #include "health-sessiond.h"
 #include "ust-app.h"
 #include "utils.h"
+#include "lttng-sessiond.h"
 
 /*
  * Send a data payload using a given consumer socket of size len.
@@ -1079,7 +1080,8 @@ int consumer_send_relayd_socket(struct consumer_socket *consumer_sock,
 		ret = relayd_create_session(rsock,
 				&msg.u.relayd_sock.relayd_session_id,
 				session_name, hostname, session_live_timer,
-				consumer->snapshot);
+				consumer->snapshot, session_id,
+				sessiond_uuid);
 		if (ret < 0) {
 			/* Close the control socket. */
 			(void) relayd_close(rsock);
