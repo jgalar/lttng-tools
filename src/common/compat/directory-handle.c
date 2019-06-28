2443336934e06687326ec2506c10e018f23df59a
Jérémie Galarneau <jeremie.galarneau@efficios.com>
2019-06-28 17:14:11 -0400

    Add rmdirat and renameat to run-as commands
    Add support for the rmdirat and renameat commands to the run-as
    infrastructure. These commands use the directory_handle compatibility
    layer to provide rename and directory deletion relative to a
    (or multiple) directory file descriptors.
    
    The rmdirat name is used even though there are no rmdirat syscall
    (at least on Linux). The functionality that would be provided by
    rmdirat() (vs rmdir) is provided through unlinkat(..., AT_REMOVEDIR).
    
    Signed-off-by: Jérémie Galarneau <jeremie.galarneau@efficios.com>

--- src/common/compat/directory-handle.c
+++ src/common/compat/directory-handle.c
@@ -21,12 +21,14 @@
 #include <common/runas.h>
 #include <common/credentials.h>
 #include <lttng/constant.h>
+#include <common/dynamic-array.h>
 
 #include <assert.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <dirent.h>
 
 /*
  * This compatibility layer shares a common "base" that is implemented
@@ -60,6 +62,30 @@ int lttng_directory_handle_unlink(
 static
 int _run_as_unlink(const struct lttng_directory_handle *handle,
 		const char *filename, uid_t uid, gid_t gid);
+static
+int _lttng_directory_handle_rename(
+		const struct lttng_directory_handle *old_handle,
+		const char *old_name,
+		const struct lttng_directory_handle *new_handle,
+		const char *new_name);
+static
+int _run_as_rename(const struct lttng_directory_handle *old_handle,
+		const char *old_name,
+		const struct lttng_directory_handle *new_handle,
+		const char *new_name, uid_t uid, gid_t gid);
+static
+DIR *lttng_directory_handle_opendir(const struct lttng_directory_handle *handle,
+		const char *path);
+static
+int lttng_directory_handle_rmdir(
+		const struct lttng_directory_handle *handle, const char *name);
+static
+int _run_as_rmdir(const struct lttng_directory_handle *handle,
+		const char *name, uid_t uid, gid_t gid);
+static
+int _run_as_rmdir_recursive(
+		const struct lttng_directory_handle *handle, const char *name,
+		uid_t uid, gid_t gid);
 static
 void lttng_directory_handle_invalidate(struct lttng_directory_handle *handle);
 
@@ -211,6 +237,76 @@ int _run_as_mkdir_recursive(const struct lttng_directory_handle *handle,
 		const char *path, mode_t mode, uid_t uid, gid_t gid)
 {
 	return run_as_mkdirat_recursive(handle->dirfd, path, mode, uid, gid);
+}
+
+static
+int _lttng_directory_handle_rename(
+		const struct lttng_directory_handle *old_handle,
+		const char *old_name,
+		const struct lttng_directory_handle *new_handle,
+		const char *new_name)
+{
+	return renameat(old_handle->dirfd, old_name,
+			new_handle->dirfd, new_name);
+}
+
+static
+int _run_as_rename(const struct lttng_directory_handle *old_handle,
+		const char *old_name,
+		const struct lttng_directory_handle *new_handle,
+		const char *new_name, uid_t uid, gid_t gid)
+{
+	return run_as_renameat(old_handle->dirfd, old_name, new_handle->dirfd,
+			new_name, uid, gid);
+}
+
+static
+DIR *lttng_directory_handle_opendir(const struct lttng_directory_handle *handle,
+		const char *path)
+{
+	DIR *dir_stream = NULL;
+	int fd = openat(handle->dirfd, path, O_RDONLY);
+
+	if (fd < 0) {
+		goto end;
+	}
+
+	dir_stream = fdopendir(fd);
+	if (!dir_stream) {
+		int ret;
+
+		PERROR("Failed to open directory stream");
+		ret = close(fd);
+		if (ret) {
+			PERROR("Failed to close file descriptor to %s", path);
+		}
+		goto end;
+	}
+
+end:
+	return dir_stream;
+}
+
+static
+int lttng_directory_handle_rmdir(
+		const struct lttng_directory_handle *handle, const char *name)
+{
+	return unlinkat(handle->dirfd, name, AT_REMOVEDIR);
+}
+
+static
+int _run_as_rmdir(const struct lttng_directory_handle *handle,
+		const char *name, uid_t uid, gid_t gid)
+{
+	return run_as_rmdirat(handle->dirfd, name, uid, gid);
+}
+
+static
+int _run_as_rmdir_recursive(
+		const struct lttng_directory_handle *handle, const char *name,
+		uid_t uid, gid_t gid)
+{
+	return run_as_rmdirat_recursive(handle->dirfd, name, uid, gid);
 }
 
 #else /* COMPAT_DIRFD */
@@ -765,4 +861,229 @@ int lttng_directory_handle_unlink_file(
 {
 	return lttng_directory_handle_unlink_file_as_user(handle,
 			filename, NULL);
+}
+
+LTTNG_HIDDEN
+int lttng_directory_handle_rename(
+		const struct lttng_directory_handle *old_handle,
+		const char *old_name,
+		const struct lttng_directory_handle *new_handle,
+		const char *new_name)
+{
+	return lttng_directory_handle_rename_as_user(old_handle, old_name,
+			new_handle, new_name, NULL);
+}
+
+LTTNG_HIDDEN
+int lttng_directory_handle_rename_as_user(
+		const struct lttng_directory_handle *old_handle,
+		const char *old_name,
+		const struct lttng_directory_handle *new_handle,
+		const char *new_name,
+		const struct lttng_credentials *creds)
+{
+	int ret;
+
+	if (!creds) {
+		/* Run as current user. */
+		ret = _lttng_directory_handle_rename(old_handle,
+				old_name, new_handle, new_name);
+	} else {
+		ret = _run_as_rename(old_handle, old_name, new_handle,
+				new_name, creds->uid, creds->gid);
+	}
+	return ret;
+}
+
+LTTNG_HIDDEN
+int lttng_directory_handle_remove_subdirectory(
+		const struct lttng_directory_handle *handle,
+		const char *name)
+{
+	return lttng_directory_handle_remove_subdirectory_as_user(handle, name,
+			NULL);
+}
+
+LTTNG_HIDDEN
+int lttng_directory_handle_remove_subdirectory_as_user(
+		const struct lttng_directory_handle *handle,
+		const char *name,
+		const struct lttng_credentials *creds)
+{
+	int ret;
+
+	if (!creds) {
+		/* Run as current user. */
+		ret = lttng_directory_handle_rmdir(handle, name);
+	} else {
+		ret = _run_as_rmdir(handle, name, creds->uid, creds->gid);
+	}
+	return ret;
+}
+
+struct rmdir_frame {
+	DIR *dir;
+	/* Size including '\0'. */
+	size_t path_size;
+};
+
+static
+void rmdir_frame_fini(void *data)
+{
+	struct rmdir_frame *frame = data;
+
+	closedir(frame->dir);
+}
+
+static
+int remove_directory_recursive(const struct lttng_directory_handle *handle,
+		const char *path)
+{
+	int ret;
+	struct lttng_dynamic_array frames;
+	size_t current_frame_idx = 0;
+	struct rmdir_frame initial_frame = {
+		.dir = lttng_directory_handle_opendir(handle, path),
+		.path_size = strlen(path) + 1,
+	};
+	struct lttng_dynamic_buffer current_path;
+	const char separator = '/';
+
+	lttng_dynamic_buffer_init(&current_path);
+	lttng_dynamic_array_init(&frames, sizeof(struct rmdir_frame),
+		        rmdir_frame_fini);
+
+	ret = lttng_dynamic_array_add_element(&frames, &initial_frame);
+	if (ret) {
+		ERR("Failed to push context frame during recursive directory removal");
+		rmdir_frame_fini(&initial_frame);
+		goto end;
+	}
+
+	ret = lttng_dynamic_buffer_append(&current_path, path,
+			initial_frame.path_size);
+        if (ret) {
+		ERR("Failed to set initial path during recursive directory removal");
+		ret = -1;
+		goto end;
+        }
+
+        while (lttng_dynamic_array_get_count(&frames) > 0) {
+		struct dirent *entry;
+		struct rmdir_frame *current_frame =
+				lttng_dynamic_array_get_element(&frames,
+						current_frame_idx);
+
+                if (!current_frame->dir) {
+			PERROR("Failed to open directory stream during recursive directory removal");
+			ret = -1;
+			goto end;
+		}
+		ret = lttng_dynamic_buffer_set_size(&current_path,
+				current_frame->path_size);
+		assert(!ret);
+		current_path.data[current_path.size - 1] = '\0';
+
+		while ((entry = readdir(current_frame->dir))) {
+			struct stat st;
+			struct rmdir_frame new_frame;
+
+			if (!strcmp(entry->d_name, ".")
+					|| !strcmp(entry->d_name, "..")) {
+				continue;
+			}
+
+			/* Set current_path to the entry's path. */
+			ret = lttng_dynamic_buffer_set_size(&current_path,
+					current_path.size - 1);
+			assert(!ret);
+			ret = lttng_dynamic_buffer_append(&current_path,
+					&separator, sizeof(separator));
+			if (ret) {
+				goto end;
+			}
+			ret = lttng_dynamic_buffer_append(&current_path,
+					entry->d_name,
+					strlen(entry->d_name) + 1);
+			if (ret) {
+				goto end;
+			}
+
+			if (lttng_directory_handle_stat(handle,
+					current_path.data, &st)) {
+				PERROR("Failed to stat \"%s\"",
+						current_path.data);
+				ret = -1;
+				goto end;
+			}
+
+			if (!S_ISDIR(st.st_mode)) {
+				/* Not empty, abort. */
+				DBG("Directory \"%s\" is not empty; refusing to remove directory",
+						current_path.data);
+				ret = -1;
+				goto end;
+			}
+
+			new_frame.path_size = current_path.size;
+			new_frame.dir = lttng_directory_handle_opendir(handle,
+					current_path.data);
+			ret = lttng_dynamic_array_add_element(&frames,
+					&new_frame);
+                        if (ret) {
+				ERR("Failed to push context frame during recursive directory removal");
+				rmdir_frame_fini(&new_frame);
+				goto end;
+                        }
+			current_frame_idx++;
+			break;
+                }
+		if (!entry) {
+			ret = lttng_directory_handle_rmdir(handle,
+					current_path.data);
+                        if (ret) {
+				PERROR("Failed to remove \"%s\" during recursive directory removal",
+						current_path.data);
+				goto end;
+                        }
+			ret = lttng_dynamic_array_remove_element(&frames,
+					current_frame_idx);
+			if (ret) {
+				ERR("Failed to pop context frame during recursive directory removal");
+				goto end;
+			}
+			current_frame_idx--;
+                }
+	}
+end:
+	lttng_dynamic_array_reset(&frames);
+	lttng_dynamic_buffer_reset(&current_path);
+	return ret;
+}
+
+LTTNG_HIDDEN
+int lttng_directory_handle_remove_subdirectory_recursive(
+		const struct lttng_directory_handle *handle,
+		const char *name)
+{
+	return lttng_directory_handle_remove_subdirectory_recursive_as_user(
+			handle, name, NULL);
+}
+
+LTTNG_HIDDEN
+int lttng_directory_handle_remove_subdirectory_recursive_as_user(
+		const struct lttng_directory_handle *handle,
+		const char *name,
+		const struct lttng_credentials *creds)
+{
+	int ret;
+
+	if (!creds) {
+		/* Run as current user. */
+		ret = remove_directory_recursive(handle, name);
+	} else {
+		ret = _run_as_rmdir_recursive(handle, name, creds->uid,
+				creds->gid);
+	}
+	return ret;
 }
